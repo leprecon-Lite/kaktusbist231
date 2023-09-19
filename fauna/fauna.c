a4539190e9f27efa32a6e784fa487ee22f35cf71
pc <pc@DESKTOP-K64F8MJ>
2023-09-19 22:53:00 +0300

    Добавьте файлы проекта.

---
+++ fauna/fauna.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <locale.h>
+
+void fauna() {
+	setlocale(LC_ALL, "RUS");
+	puts("Это кактус");
+	puts("          _x_-_          _____                ");
+	puts("          \\   /         / , - \\             ");
+	puts("         _/   \\        \\|  .  |             ");
+	puts("         |^    \\/       | ,   [              ");
+	puts("         \\_   , |       X   ' |              ");
+	puts("          |     X       |     >               ");
+	puts("          \\  <   \\      {  '  X             ");
+	puts("          |_     |      |    /                ");
+	puts("            \\_   X     < ,   |               ");
+	puts("             |    \\___/     /                ");
+	puts("              \\   ,         |                ");
+	puts("               |       , >  /                 ");
+	puts("           ____|__,_________|____             ");
+	puts("           ||                  ||             ");
+	puts("           || \\_Hello, world_/ ||            ");
+	puts("           ||                  ||             ");
+	puts("           \\|__________________|/            ");
+}
